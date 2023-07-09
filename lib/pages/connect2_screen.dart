527de5280aedf03aa9c04aaddcd90c2306fd07c2
Septian Fahmi Ardiansyah <fahrisepti888@gmail.com>
2023-07-09 13:44:46 +0700

    Fixing Page

--- lib/pages/connect2_screen.dart
+++ lib/pages/connect2_screen.dart
@@ -11,109 +11,98 @@ class Connect2Screen extends StatefulWidget {
 class _Connect2ScreenState extends State<Connect2Screen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            top: 16.0,
-            left: 25.0,
-            child: GestureDetector(
-              onTap: () {
-                // Handle button tap
-                // Add your desired functionality here
-              },
-              child: SizedBox(
-                width: 7.74,
-                height: 15.0,
-                child: Image.asset(
-                  'assets/images/back.png',
-                  fit: BoxFit.contain,
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            top: 100.0,
-            left: 25.0,
-            child: SizedBox(
-              width: 43,
-              height: 40,
-              child: Image.asset(
-                'assets/images/Group 12560.png',
-                fit: BoxFit.contain,
-              ),
-            ),
-          ),
-          Positioned(
-            top: 99.0,
-            left: 88.0,
-            child: Text(
-              'Alwatzher Series 2',
-              style: GoogleFonts.inter(
-                fontSize: 16.0,
-                fontWeight: FontWeight.w500, // "Medium" weight
-              ),
-            ),
-          ),
-          Positioned(
-            top: 120.0,
-            left: 88.0,
-            child: Text(
-              'xx:xx:xx:xx',
-              style: GoogleFonts.inter(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w500, // "Medium" weight
-                  color: Colors.grey[400]),
-            ),
-          ),
-          Positioned(
-            bottom: 120.0, // Adjust the top positioning as needed
-            left: 16.0, // Adjust the left positioning as needed
-            right: 16.0, // Adjust the right positioning as needed
-            child: SizedBox(
-              width: 283,
-              height: 40,
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Color(0xff496ADE),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-                onPressed: () {},
-                child: const Text(
-                  "Re-Scan",
-                  style: TextStyle(
-                    color: Color(0xffffffff),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 60.0, // Adjust the top positioning as needed
-            left: 137.0, // Adjust the left positioning as needed
-            right: 50.0, // Adjust the right positioning as needed
-            child: GestureDetector(
-              onTap: () {
-                // Handle link tap
-                // Add your desired functionality here, such as navigating to a specific page or performing an action
-              },
-              child: const Text(
-                'No device found?',
-                style: TextStyle(
-                  color: Color(0xff305CCF), // Set the text color to blue for the link
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w500,
-                  decoration: TextDecoration
-                      .underline, // Add underline decoration to indicate it's a link
-                ),
-              ),
-            ),
-          ),
-        ],
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: const BackButton(
+          color: Colors.black, // Set the back icon color to black
+        ),
       ),
-    ));
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Expanded(
+              child: ListView(
+                physics: const BouncingScrollPhysics(),
+                children: [
+                  Center(
+                    child: Image.asset("assets/images/Jam2.png", scale: 1.2),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 250,
+              left: 50.0,
+              right: 50.0,
+              child: Column(
+                children: [
+                  Text(
+                    "Alwatzher Series 1",
+                    style: GoogleFonts.inter(
+                        textStyle: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w500)),
+                  ),
+                  Text(
+                    "xx:xx:xx:xx",
+                    style: GoogleFonts.inter(
+                        textStyle: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w500)),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 120.0, // Adjust the top positioning as needed
+              left: 16.0, // Adjust the left positioning as needed
+              right: 16.0, // Adjust the right positioning as needed
+              child: SizedBox(
+                width: 280,
+                height: 40,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xff496ADE),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    "Re-Scan",
+                    style: TextStyle(
+                      color: Color(0xffffffff),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 80.0, // Adjust the top positioning as needed
+              left: 137.0, // Adjust the left positioning as needed
+              right: 50.0, // Adjust the right positioning as needed
+              child: GestureDetector(
+                onTap: () {
+                  // Handle link tap
+                  // Add your desired functionality here, such as navigating to a specific page or performing an action
+                },
+                child: const Text(
+                  'No device found?',
+                  style: TextStyle(
+                    color: Color(
+                        0xff305CCF), // Set the text color to blue for the link
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w500,
+                    decoration: TextDecoration
+                        .underline, // Add underline decoration to indicate it's a link
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/pages/connect_screen.dart
+++ lib/pages/connect_screen.dart
@@ -22,327 +22,145 @@ class _ConnectScreenState extends State<ConnectScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Stack(
-          children: [
-            Positioned(
-              top: 16.0,
-              left: 25.0,
-              child: GestureDetector(
-                onTap: () {
-                  // Handle button tap
-                  // Add your desired functionality here
-                },
-                child: SizedBox(
-                  width: 7.74,
-                  height: 15.0,
-                  child: Image.asset(
-                    'assets/images/back.png',
-                    fit: BoxFit.contain,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          'Add Device',
+          style: TextStyle(color: Colors.black, fontSize: 18),
+        ),
+        leading: BackButton(
+          color: Colors.black, // Set the back icon color to black
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.refresh,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 20,
+            left: 25.0,
+            child: Text(
+              'SmartWatch',
+              style: GoogleFonts.inter(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey[400] // "Medium" weight
                   ),
-                ),
-              ),
             ),
-            Positioned(
-              top: 12.5,
-              left: 45.0,
-              child: Text(
-                'Add Device',
-                style: GoogleFonts.inter(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w500, // "Medium" weight
+          ),
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 15),
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-            ),
-            Positioned(
-              top: 60,
-              left: 25.0,
-              child: Text(
-                'SmartWatch',
-                style: GoogleFonts.inter(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.grey[400] // "Medium" weight
+                ListTile(
+                  leading: Container(
+                    child: Icon(
+                      Icons.watch,
+                      color: Colors.black,
                     ),
-              ),
-            ),
-            Positioned(
-              top: 16.0,
-              right: 25.0,
-              child: GestureDetector(
-                onTap: () {
-                  // Handle button tap
-                  // Add your desired functionality here
-                },
-                child: SizedBox(
-                  width: 16.01,
-                  height: 15.0,
-                  child: Image.asset(
-                    'assets/images/refresh.png',
-                    fit: BoxFit.contain,
+                  ),
+                  title: Text(
+                    'Alwatzher Series 1',
+                    style: GoogleFonts.inter(
+                        textStyle: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w500)),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Connect2Screen()),
+                    );
+                  },
+                  trailing: Container(
+                    child: const Icon(Icons.arrow_forward_ios),
                   ),
                 ),
-              ),
-            ),
-            Positioned(
-              top: 130.0,
-              left: 25.0,
-              child: SizedBox(
-                width: 43,
-                height: 40,
-                child: Image.asset(
-                  'assets/images/Group 12560.png',
-                  fit: BoxFit.contain,
-                ),
-              ),
-            ),
-            Positioned(
-              top: 140.0,
-              left: 100.0,
-              child: Text(
-                'Alwatzher Series 1',
-                style: GoogleFonts.inter(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w500, // "Medium" weight
-                ),
-              ),
-            ),
-            Positioned(
-              top: 143.0,
-              right: 30.0,
-              child: GestureDetector(
-                onTap: () {
-                  print(1);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Connect2Screen()),
-                  );
-                },
-                child: SizedBox(
-                  width: 16.01,
-                  height: 15.0,
-                  child: Image.asset(
-                    'assets/images/Vector.png',
-                    fit: BoxFit.contain,
+                ListTile(
+                  leading: Container(
+                    child: Icon(Icons.watch, color: Colors.black),
+                  ),
+                  title: Text(
+                    'Alwatzher Series 2',
+                    style: GoogleFonts.inter(
+                        textStyle: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w500)),
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Connect2Screen()),
+                    );
+                  },
+                  trailing: Container(
+                    child: const Icon(Icons.arrow_forward_ios),
                   ),
                 ),
-              ),
+              ],
             ),
-            Positioned(
-              top: 200.0,
-              left: 25.0,
-              child: SizedBox(
-                width: 43,
-                height: 40,
-                child: Image.asset(
-                  'assets/images/Group 12560.png',
-                  fit: BoxFit.contain,
-                ),
-              ),
-            ),
-            Positioned(
-              top: 210.0,
-              left: 100.0,
-              child: Text(
-                'Alwatzher Series 2',
-                style: GoogleFonts.inter(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w500, // "Medium" weight
-                ),
-              ),
-            ),
-            Positioned(
-              top: 213.0,
-              right: 30.0,
-              child: GestureDetector(
-                onTap: () {
-                  // Handle button tap
-                  // Add your desired functionality here
-                },
-                child: SizedBox(
-                  width: 16.01,
-                  height: 15.0,
-                  child: Image.asset(
-                    'assets/images/Vector.png',
-                    fit: BoxFit.contain,
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              top: 270.0,
-              left: 25.0,
-              child: SizedBox(
-                width: 43,
-                height: 40,
-                child: Image.asset(
-                  'assets/images/Group 12560.png',
-                  fit: BoxFit.contain,
-                ),
-              ),
-            ),
-            Positioned(
-              top: 280.0,
-              left: 100.0,
-              child: Text(
-                'Alwatzher Series 3',
-                style: GoogleFonts.inter(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w500, // "Medium" weight
-                ),
-              ),
-            ),
-            Positioned(
-              top: 293.0,
-              right: 30.0,
-              child: GestureDetector(
-                onTap: () {
-                  // Handle button tap
-                  // Add your desired functionality here
-                },
-                child: SizedBox(
-                  width: 16.01,
-                  height: 15.0,
-                  child: Image.asset(
-                    'assets/images/Vector.png',
-                    fit: BoxFit.contain,
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              top: 340.0,
-              left: 25.0,
-              child: SizedBox(
-                width: 43,
-                height: 40,
-                child: Image.asset(
-                  'assets/images/Group 12560.png',
-                  fit: BoxFit.contain,
-                ),
-              ),
-            ),
-            Positioned(
-              top: 350.0,
-              left: 100.0,
-              child: Text(
-                'Alwatzher Series 4',
-                style: GoogleFonts.inter(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w500, // "Medium" weight
-                ),
-              ),
-            ),
-            Positioned(
-              top: 363.0,
-              right: 30.0,
-              child: GestureDetector(
-                onTap: () {
-                  // Handle button tap
-                  // Add your desired functionality here
-                },
-                child: SizedBox(
-                  width: 16.01,
-                  height: 15.0,
-                  child: Image.asset(
-                    'assets/images/Vector.png',
-                    fit: BoxFit.contain,
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              top: 410.0,
-              left: 25.0,
-              child: SizedBox(
-                width: 43,
-                height: 40,
-                child: Image.asset(
-                  'assets/images/Group 12560.png',
-                  fit: BoxFit.contain,
-                ),
-              ),
-            ),
-            Positioned(
-              top: 420.0,
-              left: 100.0,
-              child: Text(
-                'Alwatzher Series 5',
-                style: GoogleFonts.inter(
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w500, // "Medium" weight
-                ),
-              ),
-            ),
-            Positioned(
-              top: 433.0,
-              right: 30.0,
-              child: GestureDetector(
-                onTap: () {
-                  // Handle button tap
-                  // Add your desired functionality here
-                },
-                child: SizedBox(
-                  width: 16.01,
-                  height: 15.0,
-                  child: Image.asset(
-                    'assets/images/Vector.png',
-                    fit: BoxFit.contain,
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          showUnselectedLabels: true,
-          iconSize: 25,
-          selectedItemColor: Color(0xff526CC8),
-          selectedFontSize: 15,
-          unselectedFontSize: 10,
-          unselectedItemColor: Colors.grey,
-          currentIndex: _currentIndex,
-          onTap: (int index) {
-            setState(() {
-              _currentIndex = index;
-            });
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showUnselectedLabels: true,
+        iconSize: 25,
+        selectedItemColor: Color(0xff526CC8),
+        selectedFontSize: 15,
+        unselectedFontSize: 10,
+        unselectedItemColor: Colors.grey,
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
 
-            // Logika penanganan ketika item ditekan
-            switch (index) {
-              case 0:
-                // Ketika item "Home" ditekan
-                Navigator.pushNamed(context, '/home');
-                break;
-              case 1:
-                // Ketika item "Setting" ditekan
-                Navigator.pushNamed(context, '/setting');
-                break;
-              case 2:
-                // Ketika item "Monitor" ditekan
-                Navigator.pushNamed(context, '/monitor');
-                break;
-              case 3:
-                // Ketika item "Watch" ditekan
-                Navigator.pushNamed(context, '/watch');
-                break;
-              case 4:
-                // Ketika item "Profile" ditekan
-                Navigator.pushNamed(context, '/profile');
-                break;
-            }
-          },
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.settings), label: 'Setting'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.person_pin_circle, color: Colors.red),
-                label: 'Monitor'),
-            BottomNavigationBarItem(icon: Icon(Icons.watch), label: 'Watch'),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-          ],
-        ),
+          // Logika penanganan ketika item ditekan
+          switch (index) {
+            case 0:
+              // Ketika item "Home" ditekan
+              Navigator.pushNamed(context, '/home');
+              break;
+            case 1:
+              // Ketika item "Setting" ditekan
+              Navigator.pushNamed(context, '/setting');
+              break;
+            case 2:
+              // Ketika item "Monitor" ditekan
+              Navigator.pushNamed(context, '/monitor');
+              break;
+            case 3:
+              // Ketika item "Watch" ditekan
+              Navigator.pushNamed(context, '/watch');
+              break;
+            case 4:
+              // Ketika item "Profile" ditekan
+              Navigator.pushNamed(context, '/profile');
+              break;
+          }
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Setting'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_pin_circle, color: Colors.red),
+              label: 'Monitor'),
+          BottomNavigationBarItem(icon: Icon(Icons.watch), label: 'Watch'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
       ),
     );
   }
